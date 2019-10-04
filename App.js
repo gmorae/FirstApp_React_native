12e23387dd0b8c4e08d428807c88026a8c078f92
gmorae <gmmartins06@gmail.com>
2019-10-04 07:57:53 -0300

    Tela de login

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
-  const [value, onChangeText] = React.useState('E-mail');
   return (
 
     <View style={styles.color}>
       <View style={styles.container}>
         <View style={styles.quadradoBranco}>
           <Text style={styles.textoPrincipal}>Login</Text>
-          <TextInput style={styles.inputHome}/>
-          <TextInput style={styles.input}/>
-          <Button title="Entrar" color="#ff2b34" style={{borber: 'none', borderRadius: 10}}></Button>
+          <TextInput style={styles.inputHome} editable={true} underlineColorAndroid='transparent' placeholder={"E-mail"}/>
+          <TextInput secureTextEntry={true} style={styles.inputHome} underlineColorAndroid='transparent' placeholder={"Senha"}/>
+          <TouchableOpacity style={styles.botao} onPress={() => Alert.alert('hello')}><Text style={styles.bot}>Entrar</Text></TouchableOpacity>
         </View>
       </View>
     </View>
@@ -32,9 +31,12 @@ const styles = StyleSheet.create({
   quadradoBranco: {
     backgroundColor: '#fff',
     width: '80%',
-    height: '80%',
+    height: '100%',
     marginTop: '30%',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderRadius: 5,
+    borderColor: '#000',
+    borderWidth: 1,
   },
   textoPrincipal: {
     fontSize: 50,
@@ -44,25 +46,27 @@ const styles = StyleSheet.create({
     marginBottom: '30%',
   },
 
-
   inputHome: {
-    height: '20%',
-    width: 200,
-    color: '#ccc',
-    borderRadius: 8,
+    height: '15%',
+    width: 250,
+    borderRadius: 5,
     marginBottom: 10,
-    backgroundColor: '#000',
+    backgroundColor: '#eee',
+    color: '#000',
+    paddingLeft: 6
   }, 
-  input: {
-    height: '20%',
-    width: 200,
-    color: '#ccc',
-    borderRadius: 8,
-    marginBottom: 10,
-    backgroundColor: '#000',
-  }, 
-
   botao: {
-    borderRadius: 8,
+    borderRadius: 5,
+    backgroundColor: "#ff2b34",
+    width: '40%',
+    height: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5
+  },
+
+  bot:{
+    color: '#fff',
+    fontWeight: '500',
   },
 });
